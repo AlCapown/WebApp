0eddcda1c53f6c7d315271853881daacf77b42d8
Kyle Williams <kyle6534@yahoo.com>
2025-07-19 09:59:40 -0500

    db migration

---
+++ Database/Migrations/20250719145837_Create GameSummary Table.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGameSummaryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameAISummary",
+                columns: table => new
+                {
+                    GameAISummaryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameId = table.Column<int>(type: "int", nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", maxLength: 2147483647, nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "DATETIMEOFFSET(0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameAISummary", x => x.GameAISummaryId);
+                    table.ForeignKey(
+                        name: "FK_GameAISummary_Game_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Game",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameAISummary_GameId",
+                table: "GameAISummary",
+                column: "GameId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameAISummary");
+        }
+    }
+}
