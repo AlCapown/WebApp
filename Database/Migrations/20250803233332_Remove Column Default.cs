0d559c32bb18eff673c0e61308560a6ee997b2d0
Kyle Williams <kyle6534@yahoo.com>
2025-08-03 18:33:49 -0500

    add missing database migration

---
+++ Database/Migrations/20250803233332_Remove Column Default.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveColumnDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Ended",
+                table: "BackgroundJobLog",
+                type: "DATETIMEOFFSET(0)",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "DATETIMEOFFSET(0)",
+                oldNullable: true,
+                oldDefaultValueSql: "SYSDATETIMEOFFSET()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Ended",
+                table: "BackgroundJobLog",
+                type: "DATETIMEOFFSET(0)",
+                nullable: true,
+                defaultValueSql: "SYSDATETIMEOFFSET()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "DATETIMEOFFSET(0)",
+                oldNullable: true);
+        }
+    }
+}
